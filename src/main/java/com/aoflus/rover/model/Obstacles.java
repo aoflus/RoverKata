b229c3c7dce0566eb941ddffc9c60756f7f26e20
aoflus <alvaro.ortega.rh@gmail.com>
2020-02-17 20:50:07 +0100

    Implement obstacles

---
+++ src/main/java/com/aoflus/rover/model/Obstacles.java
@@ -0,0 +1,27 @@
+package com.aoflus.rover.model;
+
+import java.util.List;
+
+import com.aoflus.rover.utils.Coordinate;
+
+public class Obstacles {
+	
+	private List<Coordinate> obstaclesList;
+	
+	private Obstacles(List<Coordinate> obstaclesList) {
+		this.obstaclesList = obstaclesList;
+	}
+	
+	public static Obstacles generateObstacles(List<Coordinate> obstaclesList) {
+		return new Obstacles(obstaclesList);
+	}
+	
+	public boolean checkCollision(Coordinate coord) {
+		return obstaclesList.contains(coord);
+	}
+	
+	public List<Coordinate> getObstaclesList() { 
+		return this.obstaclesList;
+	}
+
+}
---
+++ src/test/java/com/aoflus/rover/utils/ObstaclesTest.java
@@ -0,0 +1,60 @@
+package com.aoflus.rover.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.aoflus.rover.model.Obstacles;
+
+public class ObstaclesTest {
+	
+	private List<Coordinate> obstaclesList;
+	
+	@Before
+	public void init() {
+		obstaclesList = new ArrayList<>();
+		obstaclesList.add(Coordinate.createCoordinate(1, 1));
+		obstaclesList.add(Coordinate.createCoordinate(3, 7));
+	}
+	
+	@Test
+	public void WeWantToCreateAListOfObstacles() {
+		// Arrange & act
+		Obstacles obstacles = Obstacles.generateObstacles(obstaclesList);
+		
+		// Assert
+		assertThat("The obstacle cannot be found.",
+				obstacles.getObstaclesList().get(0).equals(Coordinate.createCoordinate(1, 1)));
+	}
+	
+	@Test
+	public void WeWantToCheckThatACollisionIsFound() {
+		// Arrange
+		Coordinate coord = Coordinate.createCoordinate(1, 1);
+		Obstacles obstacles = Obstacles.generateObstacles(obstaclesList);
+		
+		// Act
+		boolean collision = obstacles.checkCollision(coord);
+		
+		// Assert
+		assertThat("Expected collision, but found none.", collision);
+	}
+	
+	@Test
+	public void WeWantToCheckACollisionIsNotFound() {
+		// Arrange
+		Coordinate coord = Coordinate.createCoordinate(5, 6);
+		Obstacles obstacles = Obstacles.generateObstacles(obstaclesList);
+		
+		// Act
+		boolean collision = obstacles.checkCollision(coord);
+		
+		// Assert
+		assertThat("No collision was expected, found one.", !collision);
+	}
+
+}
